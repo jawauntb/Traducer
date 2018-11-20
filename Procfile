6a60ba837c3a5e0e60f09149061776b3a44be4ac
Jawaun <jawaun.brown95@gmail.com>
2018-11-20 15:10:30 -0500

    changed server procfile

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn translation_service/api.wsgi
--- api/api/settings.py
+++ api/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kts&mnu)slhpy4$dsx1k(z6$sch1nn@$$t=#22p+v(*%e24&w)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'traducer.herokuapp.com']
 
 
 # Application definition
