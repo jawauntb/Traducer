7d1fcb8b49c0769974ffef7d472b27f98dbd8d7b
Jawaun <jawaun.brown95@gmail.com>
2018-11-26 16:01:01 -0500

    changed JSON inputs, hopefully this allows for single input with Translation model output...redeploying now

---
+++ api/build/precache-manifest.40c03af7f291bbf527044c0610f46ec0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e327b575f7a763eb5ae96db69fa43f82",
+    "url": "/static/media/translate.e327b575.svg"
+  },
+  {
+    "revision": "9b31910253cc036be23ce9d9c2bd15dc",
+    "url": "/static/media/continent.9b319102.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8f869f3eab6b3b9d4c1",
+    "url": "/static/js/main.e8f869f3.chunk.js"
+  },
+  {
+    "revision": "fed624abfaa5a1fb3634",
+    "url": "/static/js/1.fed624ab.chunk.js"
+  },
+  {
+    "revision": "e8f869f3eab6b3b9d4c1",
+    "url": "/static/css/main.8bee01ba.chunk.css"
+  },
+  {
+    "revision": "b9e4716725887559ac197b057bb674d9",
+    "url": "/index.html"
+  }
+];
