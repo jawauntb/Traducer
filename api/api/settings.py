9344ea3d49441dbcb89e6aef9a1d2c1f3df86fef
Jawaun <jawaun.brown95@gmail.com>
2018-11-26 23:18:49 -0500

    deploying this version to ensure

--- api/api/settings.py
+++ api/api/settings.py
@@ -134,6 +134,5 @@ STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'build/static'),
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'build/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build/static')
 
-# Activate Django-Heroku.
